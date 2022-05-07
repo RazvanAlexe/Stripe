47e0a7ca2e4262bac04ce6cb47c5f27fad111587
RazvanAlexe <34007818+RazvanAlexe@users.noreply.github.com>
2022-05-07 18:34:53 +0200

    Add files via upload

--- src/main/java/com.payment/Controller/CheckoutController.java
+++ src/main/java/com.payment/Controller/CheckoutController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class CheckoutController {
 
-    @Value("${STRIPE_PUBLIC_KEY}")
+    @Value("${stripe.public.key}")
     String stripePublicKey;
 
     @RequestMapping("cart/payment/checkout")
--- src/main/java/com.payment/StripeService/StripeService.java
+++ src/main/java/com.payment/StripeService/StripeService.java
@@ -18,7 +18,7 @@ import java.util.Map;
 @Service
 public class StripeService {
 
-    @Value("${STRIPE_SECRET_KEY}")
+    @Value("${stripe.apikey}")
     private String secretKey;
 
     @PostConstruct
